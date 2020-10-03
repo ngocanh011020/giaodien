0424f3b8c5ef721edf392db02e24951f3f039415
tranchi0120 <tranchi20012000@gmail.com>
2020-10-03 23:29:09 +0700

    activity

---
+++ app/src/main/java/com/example/giaodien/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.giaodien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button btback;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btback = (Button) findViewById(R.id.bt2);
+        btback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
